1525d785a58e90c35e6b05074be768e1dfb5122f
Canek <canekcambrayherrera@gmail.com>
2025-04-16 10:20:52 -0700

    Adding leaf node support and tests

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import *
 from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 def main():
   node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
@@ -7,5 +8,7 @@ def main():
 
   html_node = HTMLNode('p', 'paragraph', [], {"href": "https://www.google.com", "target": "_blank"})
   print(html_node)
+
+  print(LeafNode("p", "This is a paragraph of text.").to_html())
 
 main()
